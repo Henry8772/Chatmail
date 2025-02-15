4c271649be76b8b03d502bf7017af7a6d364bd8d
Angeladaddy <sjtusunchenge@gmail.com>
2025-02-15 15:55:26 +0000

    replace logo

--- chat-ui/src/Banner.js
+++ chat-ui/src/Banner.js
@@ -8,8 +8,8 @@ const Banner = () => {
     <div className="banner">
       <div className="banner-left">
         {/* Replace 'logo192.png' with your own logo image path if desired */}
-        <img src="logo192.png" alt="Logo" className="banner-logo" />
-        <span className="banner-username">ChatMail</span>
+        <img src="chatmail.jpg" alt="Logo" className="banner-logo" />
+        <span className="banner-username">Alice (UID:123456)</span>
       </div>
       <div className="banner-right">
         {/* Email icon routes back to the home page (/) */}
